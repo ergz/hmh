2b50915390dd548775f8819bb9af743d7673a719
ergz <e.emanuel.rodriguez@gmail.com>
2023-04-18 00:05:46 -0700

    add time stamp to build output

--- build.bat
+++ build.bat
@@ -4,3 +4,7 @@ echo --- building project: %1 ---
 
 cl /nologo /Zi /Fe:build\app.exe /Fo:build\ /Fd:build\ code/win32_handmade.cpp ^
  /link /SUBSYSTEM:WINDOWS user32.lib gdi32.lib
+
+echo.
+echo Build completed at %TIME:~0,8%
+
