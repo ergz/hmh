82cf8334ff3d103071ec36b080b24eb2ec6178a5
ergz <e.emanuel.rodriguez@gmail.com>
2023-04-19 01:06:08 -0700

    adds some new lines for print out

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 @echo off 
 
 echo --- building project: %1 ---
+echo.
 
 cl /nologo /Zi /Fe:build\app.exe /Fo:build\ /Fd:build\ code/win32_handmade.cpp ^
  /link /SUBSYSTEM:WINDOWS user32.lib gdi32.lib
 
 echo.
 echo Build completed at %TIME:~0,8%
-
+echo.
