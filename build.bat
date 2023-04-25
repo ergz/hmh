cb1f1bfdaa5fc3ab73d344b302ef396302f8b0e3
ergz <e.emanuel.rodriguez@gmail.com>
2023-04-25 01:32:48 -0700

    write and comment out clang build

--- build.bat
+++ build.bat
@@ -9,3 +9,12 @@ cl /nologo /Zi /Fe:build\app.exe /Fo:build\ /Fd:build\ code/win32_handmade.cpp ^
 echo.
 echo Build completed at %TIME:~0,8%
 echo.
+
+rem @echo off
+
+rem REM Build your application here
+rem clang++ -o build\app.exe -g code\win32_handmade.cpp -luser32 -lgdi32
+
+rem REM Print an empty line and the current time to the console in 24-hour format without milliseconds
+rem echo.
+rem echo Build completed successfully at %TIME:~0,8%
